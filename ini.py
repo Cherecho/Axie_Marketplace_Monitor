b1d6d7d2581fa218d5d98b4cb7c6f62c6f18715c
cherecho_ <serechod@gmail.com>
2022-08-05 16:39:18 +0200

    Axie Infinity API request
    Monitoring module:
    	fetch_market() added

---
+++ ini.py
@@ -0,0 +1,11 @@
+# This file will be used to run the bot
+# ATM this is usseless
+# Idea: @adriiianmartiin
+# Author: @cherecho_ and @raulfdez6
+# Start date: 2020-08-05
+
+
+if __name__ == '__main__':
+    # TODO comvert the fetch_market function to a class
+    # monitor.run()
+    pass
